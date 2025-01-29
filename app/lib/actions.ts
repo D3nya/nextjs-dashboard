02ffa415015d863086a9af7d401c74281c093bc5
Denis <51379629+D3nya@users.noreply.github.com>
2025-01-29 06:53:45 +0300

    fixed customers table

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -58,6 +58,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
           VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
   } catch (error) {
+    console.error(error);
+
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -93,6 +95,7 @@ export async function updateInvoice(id: string, prevState: State, formData: Form
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.error(error);
     return { message: "Database Error: Failed to Update Invoice." };
   }
 
